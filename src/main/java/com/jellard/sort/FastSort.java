7df342233e7f3ad86b606d177f67a17e9122810f
fengguojie <fengguojie@58.com>
2020-05-11 16:10:03 +0800

    fast sort

--- src/main/java/com/jellard/sort/FastSort.java
+++ src/main/java/com/jellard/sort/FastSort.java
@@ -6,35 +6,34 @@ public class FastSort {
 	
 	public static void main(String[] args) {
 		int[] array = new int[] { 2, 1, 4, 3, 6, 5, 8, 7 };
-		quickSort(array, 0, array.length-1);
+		fastSort(array, 0, array.length-1);
 		System.out.println(Arrays.toString(array));
 	}
-	
-	
-	private static void quickSort(int[] arr, int low, int high) {
+
+	private static void fastSort(int[] array, int low, int high) {
 		if (low < high) {
-			int index = getIndex(arr, low, high);//找寻基准数据的正确索引
-			//进行迭代对index之前和之后的数组进行相同的操作使整个数组变成有序
-			quickSort(arr, 0, index - 1);
-			quickSort(arr, index + 1, high);
+			int index = getIndex(array,low,high);
+			fastSort(array, 0, index-1);
+			fastSort(array, index+1, high);
 		}
 	}
 
-	private static int getIndex(int[] arr, int low, int high) {
-		int tmp = arr[low];// 基准数据
-		while (low < high) {
-			while (low < high && arr[high] >= tmp) {
-				high--;  //当队尾的元素大于等于基准数据时,向前挪动high指针
+	private static int getIndex(int[] array, int low, int high) {
+		int temp = array[low];
+		while(low < high) {
+			while(low < high && array[high] >= temp) {
+				high--;
 			}
-			arr[low] = arr[high];//如果队尾元素小于tmp了,需要将其赋值给low
-			while (low < high && arr[low] <= tmp) {
-				low++;//当队首元素小于等于tmp时,向前挪动low指针
+			array[low] = array[high];
+			while(low < high && array[low] <= temp) {
+				low++;
 			}
-			arr[high] = arr[low];//当队首元素大于tmp时,需要将其赋值给high
-
+			array[high] = array[low];
 		}
-		arr[low] = tmp;//跳出循环时low和high相等,此时的low或high就是tmp的正确索引位置
-		return low;// 返回tmp的正确位置
+		array[low] = temp;
+		return low;
 	}
+	
+	
 
 }
