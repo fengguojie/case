d6486b7ce5f654a62271a61a49d92ec03217daa1
fengguojie <fengguojie@58.com>
2020-09-17 16:13:09 +0800

    maxsum subarray

--- src/main/java/com/jellard/leetcode/Easy.java
+++ src/main/java/com/jellard/leetcode/Easy.java
@@ -18,11 +18,33 @@ public class Easy {
 		return slow+1;
 	}
 	
+	/**
+	 * 最大子序列
+	 * @param array
+	 * @return
+	 */
+	public int maxSum(int[] array) {
+		int maxSum = array[0];
+		int sum = array[0];
+		for (int i = 1; i < array.length; i++) {
+			if (sum+array[i] < array[i]) {
+				sum=array[i];
+			}else {
+				sum+=array[i];
+			}
+			if (sum > maxSum) {
+				maxSum = sum;
+			}
+		}
+		return maxSum;
+	}
+	
 	public static void main(String[] args) {
 		Easy easy = new Easy();
-		int[] sortedArray = {0,1,1,2,2,4,5,6,7};
-		int count = easy.count(sortedArray);
-		System.out.println(""+count);
+		int[] sortedArray = {1,1,1,2,2,4,5,6,7};
+		System.out.println("有序数组不同元素个数："+easy.count(sortedArray));
+		int[] array = {-2,1,-3,4,-1,2,1,-5,4};
+		System.out.println("最大子序列:"+easy.maxSum(array));
 	}
 	
 
